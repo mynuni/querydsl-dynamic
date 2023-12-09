cde95fbacb0e06a19acb55d3541d9fe75753d6b6
mynuni <elelaka0000@gmail.com>
2023-12-09 22:44:06 +0900

    Initial project setup

---
+++ src/main/java/com/example/querydsldynamic/QuerydslDynamicApplication.java
@@ -0,0 +1,13 @@
+package com.example.querydsldynamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslDynamicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydslDynamicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/querydsldynamic/QuerydslDynamicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.querydsldynamic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslDynamicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
